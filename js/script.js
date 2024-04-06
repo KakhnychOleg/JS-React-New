a66fb3c7ac86b0deb926a6f03adc92e369e6d6b5
Kakhnych Oleg <kakhnych.oleg@gmail.com>
2024-04-06 21:48:24 +0200

    optimaze user popup

--- js/script.js
+++ js/script.js
@@ -1,9 +1,41 @@
 'use strict';
 
 
-for (let i = 0; i < 5; i++) {
-   console.log(i);
+
+// optimaze user popup
+const numberOfFilms = +prompt("Сколько фильмов вы уже просмотрели?", "");
+
+const peronalMovieDB = {
+   count: numberOfFilms,
+   movies: {},
+   actors: {},
+   genres: [],
+   privat: false
 }
+
+for (let i = 0; i < 2; i++) {
+   let a = prompt("Один из последних просмотренных фильмов?", "");
+   let b = prompt("На сколько вы оцениваете его?", "");
+
+   if (a != '' && b != '' && a != true && b != true && a.length != 50) {
+      peronalMovieDB.movies[a] = b;
+      console.log('Ales Good!');
+   } else {
+      console.log('Problema');
+      i--;
+   }
+
+}
+
+console.log(peronalMovieDB);
+
+// const film1 = prompt("Один из последних просмотренных фильмов?", ""),
+//       film2 = prompt("На сколько вы оцениваете его?", ""),
+//       film3 = prompt("Один из последних просмотренных фильмов?", ""),
+//       film4 = prompt("На сколько вы оцениваете его?", "");
+
+// peronalMovieDB.movies[film1] = film2;
+// peronalMovieDB.movies[film3] = film4;
 
 
 // *
@@ -68,35 +100,35 @@ console.log(yelka);
 
 
 // Home
-console.log( NaN || 2 || undefined ); // 2
+// console.log( NaN || 2 || undefined ); // 2
 
-console.log( NaN && 2 && undefined ); // NaN
+// console.log( NaN && 2 && undefined ); // NaN
 
-console.log( 1 && 2 && 3 ); // 3
+// console.log( 1 && 2 && 3 ); // 3
 
-console.log( !1 && 2 || !3 ); // false
+// console.log( !1 && 2 || !3 ); // false
 
-console.log( 25 || null && !3 ); // 25
+// console.log( 25 || null && !3 ); // 25
 
-console.log( NaN || null || !3 || undefined || 5); // 5
+// console.log( NaN || null || !3 || undefined || 5); // 5
 
-console.log( NaN || null && !3 && undefined || 5); // 5?
+// console.log( NaN || null && !3 && undefined || 5); // 5?
 
-console.log( 5 === 5 && 3 > 1 || 5); // 5 || true
+// console.log( 5 === 5 && 3 > 1 || 5); // 5 || true
 
 
-const burger = 3;
-const fries = null; 
-const cola = 2;
-const nuggets = 2;
+// const burger = 3;
+// const fries = null; 
+// const cola = 2;
+// const nuggets = 2;
 
-if (burger === 3 && cola || fries && nuggets) {
-   console.log('Супер!')
-} else {
-   console.log('Уходим!')
-}
+// if (burger === 3 && cola || fries && nuggets) {
+//    console.log('Супер!')
+// } else {
+//    console.log('Уходим!')
+// }
 
-console.log(burger === 3 && cola || fries && nuggets); // 2
+// console.log(burger === 3 && cola || fries && nuggets); // 2
 
 // const burger = 5;
 // const fries = null; 
@@ -106,15 +138,15 @@ console.log(burger === 3 && cola || fries && nuggets); // 2
 // }
 
 // Работа с &&
-console.log(1 && 0); // 0
-console.log(1 && 5); // 5
-console.log(null && 5); // null
-console.log(0 && 'cola'); // 0
-// Работа с ||
-console.log(1 || 0); // 1
-console.log(1 || 5); // 1
-console.log(null || 5); // 5
-console.log(0 || 'cola'); //cola
+// console.log(1 && 0); // 0
+// console.log(1 && 5); // 5
+// console.log(null && 5); // null
+// console.log(0 && 'cola'); // 0
+// // Работа с ||
+// console.log(1 || 0); // 1
+// console.log(1 || 5); // 1
+// console.log(null || 5); // 5
+// console.log(0 || 'cola'); //cola
 
 
 // const numberOfFilms = +prompt("Сколько фильмов вы уже просмотрели?", "");
