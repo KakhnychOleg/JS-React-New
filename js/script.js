a5662718cd367bb8022fc69ed9801071a4dc4fda
kahnych.o <kahnych.o@hexide-digital.com>
2024-04-06 11:57:01 +0200

    update 1.0

--- js/script.js
+++ js/script.js
@@ -1,23 +1,84 @@
 'use strict';
 
-// alert('Hello');
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже просмотрели?", "");
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
 
-const peronalMovieDB = {
-   count: numberOfFilms,
-   movies: {},
-   actors: {},
-   genres: [],
-   privat: false
+// console.log(undefined || false);
+
+if (hamburger && cola || fries === 3 && nuggets) {
+   console.log('Done!')
 }
 
-const film1 = prompt("Один из последних просмотренных фильмов?", ""),
-      film2 = prompt("На сколько вы оцениваете его?", ""),
-      film3 = prompt("Один из последних просмотренных фильмов?", ""),
-      film4 = prompt("На сколько вы оцениваете его?", "");
 
-peronalMovieDB.movies[film1] = film2;
-peronalMovieDB.movies[film3] = film4;
+// Home
+console.log( NaN || 2 || undefined ); // 2
 
-console.log(peronalMovieDB);
+console.log( NaN && 2 && undefined ); // NaN
+
+console.log( 1 && 2 && 3 ); // 3
+
+console.log( !1 && 2 || !3 ); // false
+
+console.log( 25 || null && !3 ); // 25
+
+console.log( NaN || null || !3 || undefined || 5); // 5
+
+console.log( NaN || null && !3 && undefined || 5); // 5?
+
+console.log( 5 === 5 && 3 > 1 || 5); // 5 || true
+
+
+const burger = 3;
+const fries = null; 
+const cola = 2;
+const nuggets = 2;
+
+if (burger === 3 && cola || fries && nuggets) {
+   console.log('Супер!')
+} else {
+   console.log('Уходим!')
+}
+
+console.log(burger === 3 && cola || fries && nuggets); // 2
+
+// const burger = 5;
+// const fries = null; 
+
+// if (burger || fries) {
+//    console.log('Супер!')
+// }
+
+// Работа с &&
+console.log(1 && 0); // 0
+console.log(1 && 5); // 5
+console.log(null && 5); // null
+console.log(0 && 'cola'); // 0
+// Работа с ||
+console.log(1 || 0); // 1
+console.log(1 || 5); // 1
+console.log(null || 5); // 5
+console.log(0 || 'cola'); //cola
+
+
+// const numberOfFilms = +prompt("Сколько фильмов вы уже просмотрели?", "");
+
+// const peronalMovieDB = {
+//    count: numberOfFilms,
+//    movies: {},
+//    actors: {},
+//    genres: [],
+//    privat: false
+// }
+
+// const film1 = prompt("Один из последних просмотренных фильмов?", ""),
+//       film2 = prompt("На сколько вы оцениваете его?", ""),
+//       film3 = prompt("Один из последних просмотренных фильмов?", ""),
+//       film4 = prompt("На сколько вы оцениваете его?", "");
+
+// peronalMovieDB.movies[film1] = film2;
+// peronalMovieDB.movies[film3] = film4;
+
+// console.log(peronalMovieDB);
