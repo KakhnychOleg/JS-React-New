46d2db95944f1138bc5872b663514646a1dbb81a
Kakhnych Oleg <kakhnych.oleg@gmail.com>
2024-04-24 23:23:03 +0200

    update array

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 'use strict';
 
 // Tests ==========================================================================================
-
 const personalPlanPeter = {
    name: "Peter",
    age: "29",
@@ -12,6 +11,18 @@ const personalPlanPeter = {
            php: '10%'
        },
        exp: '1 month'
+   },
+   showAgeAndLangs: function(plan) {
+      let {age} = plan;
+      let {languages} = plan.skills;
+
+      let str = `Мне ${age} и я владею языками: `;
+
+      languages.forEach(function(lang) {
+         str += `${lang.toUpperCase()} `;
+      });
+
+      return str;
    }
 };
 
@@ -34,52 +45,6 @@ function showProgrammingLangs(plan) {
 }
 
 showProgrammingLangs(personalPlanPeter);
-
-
-// const personalPlanPeter = {
-//    name: "Peter",
-//    age: "29",
-//    skills: {
-//        languages: ['ru', 'eng'],
-//        programmingLangs: {
-//            js: '20%',
-//            php: '10%'
-//        },
-//        exp: '1 month'
-//    },
-//    showAgeAndLangs: function(plan) {
-//        const {age} = plan;
-//        const {languages} = plan.skills;
-//        let str = `Мне ${age} и я владею языками: `;
-
-//        languages.forEach(function(lang) {
-//            str += `${lang.toUpperCase()} `;
-//        });
-
-//        return str;
-//    }
-// };
-
-// personalPlanPeter.showAgeAndLangs(personalPlanPeter);
-
-// function showExperience(plan) {
-//    const {exp} = plan.skills;
-//    return exp;
-// }
-
-// showExperience(personalPlanPeter);
-
-// function showProgrammingLangs(plan) {
-//    let str = '';
-//    const {programmingLangs} = plan.skills;
-//    for (let key in programmingLangs) {
-//        str += `Язык ${key} изучен на ${programmingLangs[key]}\n`
-//    }
-
-//    return str;
-// }
-
-// showProgrammingLangs(personalPlanPeter);
 
 
 // ES 6 - ES 9 =====================================================================================
