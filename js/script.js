f5c861e7642945730ebb3e05376e3e6a2a219fda
Kakhnych Oleg <kakhnych.oleg@gmail.com>
2024-04-26 23:58:46 +0200

    test 2.0

--- js/script.js
+++ js/script.js
@@ -1,6 +1,34 @@
 'use strict';
 
-// Tests ==========================================================================================
+// Tests 2.0 =========================================================================================
+const family = ['Peter', 'Ann', 'Alex', 'Linda'];
+
+function showFamily(arr) {
+    let str = '';
+
+    arr.length === 0 ? str = 'Семья пуста' : str = 'Семья состоит из: ';
+
+    arr.forEach(member => {
+        str += `${member} `
+    });
+
+    return str;
+}
+
+showFamily(family);
+
+const favoriteCities = ['liSBon', 'ROME', 'miLan', 'Dublin'];
+
+function standardizeStrings(arr) {
+    arr.forEach(city => {
+        console.log(city.toLowerCase())
+    })
+}
+
+standardizeStrings(favoriteCities);
+
+
+// Tests =========================================================================================
 const personalPlanPeter = {
    name: "Peter",
    age: "29",
