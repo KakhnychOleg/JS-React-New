6312c6d7303d92e5fbe983abf2c3909d371d30ee
Kakhnych Oleg <kakhnych.oleg@gmail.com>
2024-05-13 23:46:10 +0200

    Closures

--- js/script.js
+++ js/script.js
@@ -1,5 +1,40 @@
 'use strict';
 
+
+// Замыкания и лексическое окружение ================================================================
+// let number = 5; debugger
+
+// function logNumber() {
+//    // let number = 4; debugger
+//    console.log(number);
+// }
+
+// number = 6;
+
+// logNumber(); debugger
+
+// number = 8;
+
+// logNumber(); debugger
+
+function createCounter() {
+   let counter = 0;
+
+   const myFunction = function() {
+      counter = counter + 1;
+      return counter;
+   }
+
+   return myFunction;
+}
+
+const increment = createCounter();
+const c1 = increment();
+const c2 = increment();
+const c3 = increment();
+
+ console.log(c1, c2, c3);
+
 // Динамическая типизация ============================================================================
 
 
