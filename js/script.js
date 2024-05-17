200f3678251748ccab213e940d74c02b2589fba7
Kakhnych Oleg <kakhnych.oleg@gmail.com>
2024-05-17 22:30:24 +0200

    search error

--- js/script.js
+++ js/script.js
@@ -1,5 +1,65 @@
 'use strict';
 
+// test 3.0 ========================================================================================
+// Search error in code
+const restorantData = {
+   menu: [
+      {
+      name: 'Salad Caesar',
+      price: '14$'
+      },
+      {
+      name: 'Pizza Diavola',
+      price: '9$'
+      },
+      {
+      name: 'Beefsteak',
+      price: '17$'
+      },
+      {
+      name: 'Napoleon',
+      price: '7$'
+      }
+   ],
+   waitors: [
+      {name: 'Alice', age: 22}, 
+      {name: 'John', age: 24}
+   ],
+   averageLunchPrice: '20$',
+   openNow: true
+};
+
+function isOpen(prop) {
+   let answer = '';
+   prop ? answer = 'Открыто' : answer = 'Закрыто';
+
+   return answer;
+}
+// console.log(restorantData.openNow);
+console.log(isOpen(restorantData.openNow));
+
+function isAverageLunchPriceTrue(fDish, sDish, average) {
+   if (+fDish.price.slice(0, -1) + (+sDish.price.slice(0, -1)) < +average.slice(0,-1)) {
+      return 'Цена ниже средней';
+   } else {
+      return 'Цена выше средней';
+   }
+}
+
+console.log(isAverageLunchPriceTrue(restorantData.menu[0], restorantData.menu[1], restorantData.averageLunchPrice));
+
+function transferWaitors(data) {
+   const copy = Object.assign({}, data);
+
+   // copy.waitors = {name: 'Mike', age: 32}; 
+   copy.waitors = [{name: 'Mike', age: 32}];
+   return copy; 
+}
+
+transferWaitors(restorantData); 
+// console.log(transferWaitors(restorantData.waitors));
+
+
 // Повтор, тесты ====================================================================================
 // 1)
 // let x = 5;
