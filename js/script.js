c219033bf7d9ffef129bc01fbe8d0d35434528fc
Kakhnych Oleg <kakhnych.oleg@gmail.com>
2024-05-26 12:18:05 +0200

    work with page elements

--- js/script.js
+++ js/script.js
@@ -1,15 +1,64 @@
 'use strict';
+// ================================================================================================
+/*
+1) Удалить все рекламные блоки со страницы (правая часть сайта)
+
+2) Изменить жанр фильма, поменять "комедия" на "драма"
+
+3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+Реализовать только при помощи JS
+
+4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+Отсортировать их по алфавиту 
+
+5) Добавить нумерацию выведенных фильмов 
+*/
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против..."
+    ]
+};
+
+const adv = document.querySelectorAll('.promo__adv img');
+const poster = document.querySelector('.promo__bg');
+const genre = poster.querySelector('.promo__genre');
+const movieList = document.querySelector('.promo__interactive-list');
+
+adv.forEach(item => {
+    item.remove();
+});
+
+genre.textContent = 'Драма';
+
+poster.style.backgroundImage = 'url("img/bg.jpg")';
+
+movieList.innerHTML = '';
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML += `
+        <li class="promo__interactive-item">${i + 1}. ${film}
+            <div class="delete"></div>
+        </li>
+    `;
+});
+
 
 // ================================================================================================
 //  const someName = document.getElementById('');
-const box = document.getElementById('box');
-const boxTwo = document.getElementsByClassName('boxTwo');
+// const box = document.getElementById('box');
+// const boxTwo = document.getElementsByClassName('boxTwo');
 
-box.style.backgroundColor = 'blue';
-box.style.width = '100px';
-box.style.height = '100px';
+// box.style.backgroundColor = 'blue';
+// box.style.width = '100px';
+// box.style.height = '100px';
 
-boxTwo[0].style.color = 'red';
+// boxTwo[0].style.color = 'red';
 // Для моного строчных стилей
 // box.style.cssText = 'background-color: red; width: 200px';
 // box.style.cssText = `background-color: red; width: ${paramWidth}`;
