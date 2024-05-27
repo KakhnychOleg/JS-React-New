40256595ea1d9194b9b96333afbbdc6a579a5cbe
Kakhnych Oleg <kakhnych.oleg@gmail.com>
2024-05-27 23:40:21 +0200

    event DOM

--- js/script.js
+++ js/script.js
@@ -1,8 +1,32 @@
 'use strict';
 
 // ================================================================================================
+// DOM
+// console.log(document.head);
+// console.log(document.documentElement);
+// console.log(document.body.firstChild);
+// console.log(document.body.lastChild);
+
+
+// console.log(document.querySelector('#btn').parentNode);
+// console.log(document.querySelector('#btn').parentNode.parentNode);
+
+// console.log(document.querySelector('[data-current="3"]').nextSibling); //next elem  
+// console.log(document.querySelector('[data-current="3"]').previousSibling); //prev elem
+// console.log(document.querySelector('[data-current="3"]').nextElementSibling);
+
+// for (let node of document.body.childNodes) {
+//    if (node.nodeName == '#text') {
+//       continue;
+//    }
+   
+//    console.log(node);
+// };
+
+
+// ================================================================================================
 // const btn = document.querySelector('button');
-const btns = document.querySelector('button');
+// const btns = document.querySelector('button');
 
 // Не рекомендуемый формат для записи
 // btn.onclick = function() {
