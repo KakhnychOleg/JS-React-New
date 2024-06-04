f0541fbfda6e0baf211d161e21f74c928d02135c
Kakhnych Oleg <kakhnych.oleg@gmail.com>
2024-06-04 22:40:47 +0200

    html collection

--- js/script.js
+++ js/script.js
@@ -2,8 +2,23 @@
 
 // =======================================================================================================
 // Оператор опциональной цепочки (?.)
-const box = document.querySelector('.box');
-const block = document.querySelector('.block');
+const boxesQuery = document.querySelectorAll('.box');
+const boxesGet = document.getElementsByClassName('box');
+
+boxesQuery.forEach(box => {
+   if (box.matches('.this')) {
+      console.log(box);
+   } 
+}); 
+
+// boxesQuery[0].remove();
+// boxesGet[0].remove();
+
+// console.log(boxesQuery);
+// console.log(boxesGet);
+// console.log(document.body.children);
+
+// console.log(Array.from(boxesGet));
 
 // console.log(block);
 
@@ -13,22 +28,22 @@ const block = document.querySelector('.block');
 
 // console.log(block?.textContent);
 // console.log(1 + 2);
-const userData = {
-   name: 'Ivan',
-   age: null,
-   say: function() {
-      console.log('Hello');
-   }
-};
+// const userData = {
+//    name: 'Ivan',
+//    age: null,
+//    say: function() {
+//       console.log('Hello');
+//    }
+// };
 
-userData.say();
-userData.hey?.();
+// userData.say();
+// userData.hey?.();
 
 // if (userData && userData.skills && userData.skills.js) {
 //    console.log(userData.skills.js);
 // }
 
-console.log(userData?.skills?.js);
+// console.log(userData?.skills?.js);
 
 
 // =======================================================================================================
