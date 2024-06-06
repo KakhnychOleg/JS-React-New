2ed994aa3bb87b501ce36e86046812ef2cbd3449
Kakhnych Oleg <kakhnych.oleg@gmail.com>
2024-06-06 19:51:59 +0200

    object.values

--- js/script.js
+++ js/script.js
@@ -1,6 +1,36 @@
 'use strict';
 
 // =======================================================================================================
+const user = {
+   name: 'Alex',
+   surname: 'Smith',
+   birthday: '20/04/1994',
+   showMyPublickData: function() {
+      console.log(`${this.name} ${this.surname}`);
+   }
+}
+// Object.defineProperty(user, 'birthday', {writable: false});
+// Object.defineProperty(user, 'birthday', {value: prompt('Date?'), enumerable: true, configurable: true});
+
+console.log(Object.getOwnPropertyDescriptor(Math, 'PI'));
+
+Object.defineProperty(user, 'showMyPublicData', {enumerable: false});
+
+for (let key in user) {
+   console.log(key);
+}
+
+Object.defineProperties(user, {
+   name: {writable: false},
+   surname: {writable: false}
+});
+
+// writable
+// enumerable
+// configurable
+
+
+// =======================================================================================================
 // Symbol
 const obj = {
    'name': 'Test',
